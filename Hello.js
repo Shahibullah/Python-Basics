5495d328502911b99f1dc95ae058e9dec4084888
Shahib <163203412+Shahibullah@users.noreply.github.com>
2025-05-27 18:25:08 +0600

    Add files via upload

---
+++ Hello.js
@@ -0,0 +1,14 @@
+
+let a = 10;
+let b = 20; 
+let answer =  a + b ;
+console.log("Sum will be " + answer);
+const name = "Shahib";
+console.log("My name is " + name);
+console.log(typeof name);
+const my_list = ["AHMMED SHAHIBULLAH",22301025,"CSE","BRAC University",true];
+console.log(my_list);
+my_list.push("JSON")
+console.log(my_list)
+my_list.pop()
+console.log(my_list)
